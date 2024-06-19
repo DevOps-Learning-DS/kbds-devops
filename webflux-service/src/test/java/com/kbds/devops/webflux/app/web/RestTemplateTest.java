bb3a21d99de1148d316d4fb66132c96f8798cc14
ParkBoKyoung <linuxpbk@gmail.com>
2024-06-19 11:30:26 +0900

    Completing HttClient

--- webflux-service/src/test/java/com/kbds/devops/webflux/app/web/RestTemplateTest.java
+++
@@ -1,9 +0,0 @@
-package com.kbds.devops.webflux.app.web;
-
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class RestTemplateTest {
-
-}
