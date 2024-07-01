788f364cfd808f4f5dc40df51573b9215a5293e7
BK Park <linuxpbk@gmail.com>
2024-07-01 21:31:02 +0900

    Add MemberInfo

--- memberinfo-service/build.gradle.kts
+++ memberinfo-service/build.gradle.kts
@@ -1,19 +1,11 @@
-plugins {
-    id("java")
-}
-
-group = "com.kbds.devops"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
+    val jsonFormatJsrVer = rootProject.extra["jsonFormatJsr"]
+    val embeddedMongoVer = rootProject.extra["embeddedMongo"]
 
-tasks.test {
-    useJUnitPlatform()
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jsonFormatJsrVer")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:${embeddedMongoVer}")
 }
---
+++ memberinfo-service/src/main/java/com/kbds/devops/memberinfo/app/MemberInfoApplication.java
@@ -0,0 +1,12 @@
+package com.kbds.devops.memberinfo.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberInfoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberInfoApplication.class, args);
+    }
+}
